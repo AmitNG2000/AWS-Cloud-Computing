04b964b53cb83a8e46d66ae5ed63fcf8905a5825
Amit Ner-Gaon <131769038+AmitNG2000@users.noreply.github.com>
2024-12-24 10:14:11 +0200

    coding step 1

--- Assignment2/Hadoop/src/main/java/Step1.java
+++ Assignment2/Hadoop/src/main/java/Step1.java
@@ -1,5 +1,8 @@
+package main.java;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -19,20 +22,62 @@ import java.util.StringTokenizer;
  * @Input split from a text file
  * @Output: ((w1), <LongWritable>), ((w1,w2), <LongWritable>), ((w1,w2,w3), <LongWritable>)
  */
-public class Step1 {
+public class Step1Count {
 
-    public static class MapperClass extends Mapper<LongWritable, Text, MyClasses.TextArrayWritable, LongWritable> {
+    //helper classes
+    public class TextArrayWritable extends ArrayWritable {
+            public TextArrayWritable() {
+                super(Text.class);  // Specify the type of elements
+            }
+        }
+    private TextArrayWritable outputArrayWritable = new TextArrayWritable(); // helper classes for mapper
+
+    public static class MapperClass extends Mapper<LongWritable, Text, TextArrayWritable, LongWritable> {
         private final static IntWritable one = new IntWritable(1);
+        private Text w1 = new Text();
+        private Text w2 = new Text();
+        private Text w3 = new Text();
 
         @Override
         public void map(LongWritable key, Text sentence, Context context) throws IOException,  InterruptedException {
-        //TODO: comtinue from here
+        //TODO: continue from here
 
-            StringTokenizer itr = new StringTokenizer(sentence.toString());
-            while (itr.hasMoreTokens()) {
-                word.set(itr.nextToken());
-                context.write(word, one);
-            }
+            // Tokenize the input sentence
+            StringTokenizer tokenizer = new StringTokenizer(sentence.toString());
+
+            // Store the words for constructing n-grams
+            String[] words = new String[3];
+            int count = 0;
+
+            while (tokenizer.hasMoreTokens()) {
+                // Shift words to construct n-grams
+                words[count % 3] = tokenizer.nextToken();
+                count++;
+
+                if (count >= 1) {
+                    // Emit uni-gram (w1)
+                    w1.set(words[(count - 1) % 3]);
+                    Text[] output = new Text[]{w1};
+                    context.write(outputArrayWritable.set(output), one);
+                }
+
+                if (count >= 2) {
+                    // Emit bi-gram (w1, w2)
+                    w1.set(words[(count - 2) % 3]);
+                    w2.set(words[(count - 1) % 3]);
+                    Text[] output = new Text[]{w1, w2};
+                    context.write(outputArrayWritable.set(output), one);
+                }
+
+                if (count >= 3) {
+                    // Emit tri-gram (w1, w2, w3)
+                    w1.set(words[(count - 3) % 3]);
+                    w2.set(words[(count - 2) % 3]);
+                    w3.set(words[(count - 1) % 3]);
+                    Text[] output = new Text[]{w1, w2, w3};
+                    context.write(outputArrayWritable.set(output), one);
+                }
+            } //end of while
         }
     }
 
@@ -59,7 +104,7 @@ public class Step1 {
         System.out.println(args.length > 0 ? args[0] : "no args");
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "Word Count");
-        job.setJarByClass(Step1.class);
+        job.setJarByClass(Step1Count.class);
         job.setMapperClass(MapperClass.class);
         job.setPartitionerClass(PartitionerClass.class);
         job.setCombinerClass(ReducerClass.class);
@@ -70,7 +115,7 @@ public class Step1 {
         job.setOutputValueClass(IntWritable.class);
 
 //        For n_grams S3 files.
-//        Note: This is English version and you should change the path to the relevant one
+//        Note: This is English version,and you should change the path to the relevant one
 //        job.setOutputFormatClass(TextOutputFormat.class);
 //        job.setInputFormatClass(SequenceFileInputFormat.class);
 //        TextInputFormat.addInputPath(job, new Path("s3://datasets.elasticmapreduce/ngrams/books/20090715/eng-us-all/3gram/data"));
